a265d50f4e6fb757e7aae8390603629bc8f5b8c0
elanonimotranquilo <elanonimotranquilo@gmail.com>
2022-11-19 00:27:06 +0100

    Add files via upload

---
+++ gnl_25_paco_normal/get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalas <asalas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 23:30:00 by asalas-s          #+#    #+#             */
+/*   Updated: 2022/11/18 04:52:07 by asalas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read_fd(int fd, int *n_read);
+char	*ft_get_line(const char *str);
+char	*ft_save_line(const char *str);
+char	*ft_read_buffer(int fd, int *n_read);
+
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+#endif
