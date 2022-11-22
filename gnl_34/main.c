e5b30ad320e030b17f629f4ae626e1aab1107b5a
elanonimotranquilo <elanonimotranquilo@gmail.com>
2022-11-22 08:52:25 +0100

    Add files via upload

---
+++ gnl_34/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalas <asalas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 20:00:32 by asalas-s          #+#    #+#             */
+/*   Updated: 2022/11/22 07:12:09 by asalas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*gnl;
+
+	(void)argc;
+	fd = open (argv[1], O_RDONLY);
+	gnl = "";
+	while (gnl)
+	{
+		gnl = get_next_line (fd);
+		printf("\n*|%s|*\n", gnl);
+	}
+	fd = close(fd);
+	return (0);
+}
