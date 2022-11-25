3c160f0ba8c0fc53ab5bc37d8f0c372095576f92
elanonimotranquilo <elanonimotranquilo@gmail.com>
2022-11-25 01:26:58 +0100

    Add files via upload

---
+++ gnl_44_pnok_psto/get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalas-s <asalas-s@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 18:15:40 by asalas-s          #+#    #+#             */
+/*   Updated: 2022/11/24 21:36:45 by asalas-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+void	*ft_free_clean(char	**str);
+char	*get_next_line(int fd);
+char	*ft_read_fd(int fd, int *n_read, char **sline);
+char	*ft_get_line(const char *str);
+char	*ft_save_line(const char *str);
+
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s);
+
+#endif
